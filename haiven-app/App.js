35bd01344027746ea34addf190fda76c8a9214ba
jenny-huaang <40249176+jenny-huaang@users.noreply.github.com>
2020-07-13 12:48:45 -0700

    main screen rough implementation

--- haiven-app/App.js
+++ haiven-app/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import FeatureScreen from "./app/screens/FeatureScreen";
+import MainScreen from "./app/screens/MainScreen";
 
 export default function App() {
-  return <FeatureScreen />;
+  return <MainScreen />;
 }
---
+++ haiven-app/app/screens/MainScreen.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { SafeAreaView, View, StyleSheet, Text, Image } from "react-native";
+import colors from "../config/colors";
+import { Ionicons } from "@expo/vector-icons";
+
+export class Section extends React.Component {
+  render() {
+    return (
+      <View style={styles.button}>
+        <Ionicons name={this.props.icon} size={50} color={colors.tertiary} />
+        <Text style={styles.text}>{this.props.description}</Text>
+      </View>
+    );
+  }
+}
+
+function MainScreen(props) {
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <Image source={require("../assets/logo.png")} />
+      </View>
+      <View style={styles.layout}>
+        <Section icon="md-calendar" description="Calendar" />
+        <Section icon="md-book" description="Journal" />
+        <Section icon="ios-chatboxes" description="Check-In" />
+        <Section icon="md-help-circle-outline" description="Support" />
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  header: {
+    height: "40%",
+    width: "100%",
+    backgroundColor: colors.primary,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  layout: {
+    flexWrap: "wrap",
+    flexDirection: "row",
+  },
+  button: {
+    backgroundColor: colors.primary,
+    padding: 15,
+    borderRadius: 10,
+    margin: 20,
+    width: "40%",
+    alignItems: "flex-start",
+    justifyContent: "center",
+  },
+  text: {
+    color: "#fff",
+    fontWeight: "bold",
+    marginTop: 15,
+  },
+});
+
+export default MainScreen;
