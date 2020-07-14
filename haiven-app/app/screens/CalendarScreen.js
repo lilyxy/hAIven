0902b6a128d43184492d532c2ad6d183d4333a20
jenny-huaang <40249176+jenny-huaang@users.noreply.github.com>
2020-07-14 12:28:57 -0700

    basic calendar view

--- haiven-app/app/screens/CalendarScreen.js
+++ haiven-app/app/screens/CalendarScreen.js
@@ -1,8 +1,37 @@
-import React from "React";
-import { View } from "react-native";
+import React from "react";
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import colors from "../config/colors";
 
 function CalendarScreen(props) {
-  return <View></View>;
+  return (
+    <SafeAreaView style={styles.container}>
+      <View>
+        <Calendar
+          style={{
+            alignSelf: "stretch",
+          }}
+          theme={{
+            textSectionTitleColor: colors.primary,
+            monthTextColor: colors.primary,
+            indicatorColor: "blue",
+            selectedDotColor: colors.secondary,
+            selectedDayTextColor: colors.secondary,
+            todayTextColor: colors.primary,
+            arrowColor: colors.primary,
+            textMonthFontWeight: "bold",
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+  },
+});
 
 export default CalendarScreen;
