8db77fd5ed3f80be8879b3d6dbf0a8e20a213f8a
jenny-huaang <40249176+jenny-huaang@users.noreply.github.com>
2020-07-15 21:49:57 -0700

    Container class created

---
+++ haiven-app/app/components/Container.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import colors from "../config/colors";
+
+export class Container extends React.Component {
+  render() {
+    return <View style={styles.container}></View>;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.background,
+  },
+});
