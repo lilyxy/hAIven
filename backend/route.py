34aedcaeccce0058dcdd00708ae95f93ef922ad2
Elaine Lau <elaine@Elaines-MacBook-Pro.local>
2020-07-19 01:44:40 -0400

    testing

--- backend/route.py
+++ backend/route.py
@@ -1,7 +1,29 @@
 
 import os 
-from flask import Flask
+from flask import Flask, request
+from flask_cors import CORS, cross_origin
+from database import *
 app = Flask(__name__)
+
+cors = CORS(app, support_credentials=True)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+@app.route('/home', methods=['GET', 'POST'])
+@cross_origin(support_credentials=True)
+def login():
+    if request.method == 'POST': 
+        username = request.get_json()['username']
+        password = request.get_json()['password']
+        response = query_user(username)
+        if not response:
+            return "Does not exist"
+        password_db = response[0]["password"]
+        if password != password_db:
+            return "Wrong password"
+        return username
+    elif request.method == 'GET':
+        return "IN GET REQUEST"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
