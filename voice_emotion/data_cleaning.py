e0fd1d711ff285efe38c4a70209020dfcc2d32d7
michaa1 <alexi.michael.13@gmail.com>
2020-07-21 16:46:32 -0400

    Finalized Emotion Classification Model
    Saved keras emotion classification model (emotion_cnn.pkl)
    
    Tested the model (85.1% accuracy)

--- voice_emotion/data_cleaning.py
+++ voice_emotion/data_cleaning.py
@@ -1,4 +1,4 @@
-
+from sklearn.model_selection import train_test_split
 import librosa
 import numpy as np
 import pandas as pd
@@ -13,17 +13,18 @@ import glob
 # Checks a rolling average of signal over 1/10 sec and compares to threshold
 # Returns a mask of True and False values that can be used to filter audio signals
 
+
 def envelope(y, sr, threshold):
     mask = []
     y_abs = pd.Series(y).apply(np.abs)
-    y_mean = y_abs.rolling(window = int(sr/10), min_periods = 1, center = True).mean()
+    y_mean = y_abs.rolling(window=int(
+        sr/10), min_periods=1, center=True).mean()
     for mean in y_mean:
         if mean > threshold:
             mask.append(True)
         else:
             mask.append(False)
     return np.array(y[mask])
-
 
 
 def clean_files(file_list):
@@ -34,47 +35,51 @@ def clean_files(file_list):
         y, sr = librosa.load(file)
         y = envelope(y, sr, 0.0005)
         save_file = 'clean/' + file
-        
+
         if not os.path.exists(os.path.dirname(save_file)):
             try:
                 os.makedirs(os.path.dirname(save_file))
-            except OSError as exc: # Guard against race condition
+            except OSError as exc:  # Guard against race condition
                 if exc.errno != errno.EEXIST:
                     raise
-        
+
         with open(save_file, 'w') as new_file:
             sf.write(save_file, y, sr)
             new_file.close()
-            
+
         count += 1
         if count % 100 == 0:
             print('cleaned and saved 100 files')
-    
+
     print("cleaning complete!")
 
 #ravdess_file_list = glob.glob('RAVDESS/*/*.wav')
-#clean_files(ravdess_file_list)
+# clean_files(ravdess_file_list)
 
 #tess_file_list = glob.glob('TESS/*/*.wav')
-#clean_files(tess_file_list)
+# clean_files(tess_file_list)
 
 #savee_file_list = glob.glob('SAVEE/*/*/*.wav')
-#clean_files(savee_file_list)
+# clean_files(savee_file_list)
 
 # Now that all of the files have been cleaned lets create a simple csv with filenames and basic features
 # Define function for building the RAVDESS index
+
+
 def build_ravdess_index(file_list):
-    
-    emotion_key = {'01': 'neutral', '02': 'calm', '03': 'happy', '04': 'sad', '05': 'angry', '06': 'fearful', '07': 'disgusted', '08': 'surprised'}
+
+    emotion_key = {'01': 'neutral', '02': 'calm', '03': 'happy', '04': 'sad',
+                   '05': 'angry', '06': 'fearful', '07': 'disgusted', '08': 'surprised'}
     intensity_key = {'01': 'normal', '02': 'strong'}
 
-    df = {'dataset': [], 'filename': [], 'actor': [], 'emotion': [], 'intensity': [], 'statement': [], 'repitition': [], 'length': [], 'gender': []}
+    df = {'dataset': [], 'filename': [], 'actor': [], 'emotion': [], 'intensity': [
+    ], 'statement': [], 'repitition': [], 'length': [], 'gender': []}
 
     for file in file_list:
-        
+
         file = file.replace('\\', '/')
         df['dataset'].append('RAVDESS')
-        
+
         df['filename'].append(file)
 
         props = file.split('/')[3].split('.')[0].split('-')
@@ -93,26 +98,30 @@ def build_ravdess_index(file_list):
         df['length'].append(y.shape[0]/sr)
 
     file_properties = pd.DataFrame(df)
-    
+
     return file_properties
+
 
 ravdess_clean_list = glob.glob('clean/RAVDESS/*/*.wav')
 ravdess_index = build_ravdess_index(ravdess_clean_list)
-    
-# Define function for building the SAVEE index
-def build_savee_index(file_list):
-    
-    emotion_key = {'n': 'neutral', 'h': 'happy', 'sa': 'sad', 'a': 'angry', 'f': 'fearful', 'd': 'disgusted', 'su': 'surprised'}
 
-    df = {'dataset': [], 'filename': [], 'actor': [], 'emotion': [], 'repitition': [], 'length': [], 'gender': []}
+# Define function for building the SAVEE index
+
+
+def build_savee_index(file_list):
+
+    emotion_key = {'n': 'neutral', 'h': 'happy', 'sa': 'sad',
+                   'a': 'angry', 'f': 'fearful', 'd': 'disgusted', 'su': 'surprised'}
+
+    df = {'dataset': [], 'filename': [], 'actor': [],
+          'emotion': [], 'repitition': [], 'length': [], 'gender': []}
 
     for file in file_list:
         df['dataset'].append('SAVEE')
-        
+
         file = file.replace('\\', '/')
-        
+
         df['filename'].append(file)
-        
 
         props = file.split('/')
         df['actor'].append(props[4])
@@ -124,27 +133,29 @@ def build_savee_index(file_list):
         df['length'].append(y.shape[0]/sr)
 
     file_properties = pd.DataFrame(df)
-    
+
     return file_properties
- 
-savee_clean_list = glob.glob('clean/SAVEE/Data/AudioData/*/*.wav')   
+
+
+savee_clean_list = glob.glob('clean/SAVEE/Data/AudioData/*/*.wav')
 savee_index = build_savee_index(savee_clean_list)
 
 
 # Define function for building the TESS index
 
 def build_tess_index(file_list):
-    
+
     emotion_key = {'neutral': 'neutral', 'happy': 'happy', 'sad': 'sad', 'angry': 'angry',
                    'fear': 'fearful', 'disgust': 'disgusted', 'ps': 'surprised'}
 
-    df = {'dataset': [], 'filename': [], 'actor': [], 'emotion': [], 'statement': [], 'length': [], 'gender': []}
+    df = {'dataset': [], 'filename': [], 'actor': [],
+          'emotion': [], 'statement': [], 'length': [], 'gender': []}
 
     for file in file_list:
-        
+
         file = file.replace('\\', '/')
         df['dataset'].append('TESS')
-        
+
         df['filename'].append(file)
         props = file.split('/')[3].split('_')
         df['actor'].append(props[0])
@@ -156,8 +167,9 @@ def build_tess_index(file_list):
         df['length'].append(y.shape[0]/sr)
 
     file_properties = pd.DataFrame(df)
-    
+
     return file_properties
+
 
 tess_clean_list = glob.glob('clean/TESS/*/*.wav')
 tess_index = build_tess_index(tess_clean_list)
@@ -165,24 +177,26 @@ tess_index = build_tess_index(tess_clean_list)
 # Now combine all the dataset indexes into one using their common columns
 common_cols = ['dataset', 'filename', 'actor', 'emotion', 'length', 'gender']
 
-complete_index = pd.concat([ravdess_index[common_cols], savee_index[common_cols], tess_index[common_cols]], axis = 0)
-complete_index.reset_index(drop = True, inplace = True)
+complete_index = pd.concat(
+    [ravdess_index[common_cols], savee_index[common_cols], tess_index[common_cols]], axis=0)
+complete_index.reset_index(drop=True, inplace=True)
 
 # drop calm because there is not enough data
-complete_index = complete_index.drop(list(complete_index[complete_index['emotion'] == 'calm'].index), axis = 0).reset_index(drop = True)
+complete_index = complete_index.drop(list(
+    complete_index[complete_index['emotion'] == 'calm'].index), axis=0).reset_index(drop=True)
 
 # Lets split our dataset into training, validation, and test sets
-from sklearn.model_selection import train_test_split
 
 
 # For now I'm not going to worry about using a validation set.
 # Later if I want to more finely tune my hyper parameters I just need to uncomment some lines to also get a validation set.
 def assign_sets(complete_index):
-    train, test = train_test_split(complete_index, test_size = 0.2, random_state = 42)
+    train, test = train_test_split(
+        complete_index, test_size=0.2, random_state=42)
     #validation, test = train_test_split(_test, test_size = 0.5, random_state = 42)
-    
+
     set_list = []
-    
+
     for file in complete_index['filename']:
         if file in list(train['filename']):
             set_list.append('train')
@@ -190,9 +204,9 @@ def assign_sets(complete_index):
 #             set_list.append('validation')
         if file in list(test['filename']):
             set_list.append('test')
-    
+
     complete_index['set'] = set_list
-    
+
     return complete_index
 
 
---
+++ voice_emotion/final_model/train_model.py
@@ -0,0 +1,160 @@
+import numpy as np
+import pandas as pd
+import librosa
+import pickle
+
+from tqdm import tqdm
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPool2D, Flatten, Dropout, Dense
+from sklearn.utils.class_weight import compute_class_weight
+
+
+# Download csv file
+file_properties = pd.read_csv('voice_emotion/complete_index.csv')
+
+# Set train data
+train = file_properties[file_properties['set'] == 'train']
+train.drop('Unnamed: 0', inplace=True, axis=1)
+
+# Set emotion classes
+classes = pd.DataFrame(
+    {'emotion': ['neutral', 'happy', 'sad', 'angry', 'fearful', 'disgusted', 'surprised']})
+
+
+# Create a configuration class
+class Config:
+    def __init__(self, n_mfcc=26, n_feat=13, n_fft=552, sr=22050, window=0.4, test_shift=0.1):
+        self.n_mfcc = n_mfcc
+        self.n_feat = n_feat
+        self.n_fft = n_fft
+        self.sr = sr
+        self.window = window
+        self.step = int(sr * window)
+        self.test_shift = test_shift
+        self.shift = int(sr * test_shift)
+
+
+config = Config()
+
+
+# Set the training data
+def build_train_feats():
+
+    print('''
+    -------- Loading Train Data --------
+    ''')
+
+    X = []
+    y = []
+
+    for row in tqdm(train.iterrows(), total=3238, ncols=100, desc='Training Data'):
+
+        # Initialize min and max values for each file for scaling
+        _min, _max = float('inf'), -float('inf')
+
+        # Load the file
+        '''
+        .wav file: file format for storing audio bitstream, uncompressed audio in LPCM format
+        librosa.load(filename): filename -> string variable containing the path to the audio file
+        wav: time series -> 1D numpy floating point array
+        sr: sampling rate -> # of samples per second of audio, default = 22050 Hz
+        '''
+
+        wav, sr = librosa.load('voice_emotion/' + row[1]['filename'])
+
+        # Create an array to hold features for each window generated from specific file
+        Xf = []
+
+        # Create randomly selected 0.4s windows from each file. Select 1 random window for every 0.1s of audio in file.
+        n_samples = int(10 * float(row[1]['length']))
+
+        for i in range(n_samples):
+            # Get the numerical label for the emotion of the file
+            # This needs to be in for loop so that y.shape and X.shape match up
+            y.append(classes[classes['emotion'] == row[1]['emotion']].index[0])
+
+            # choose random starting point for window
+            rand_ind = np.random.randint(0, wav.shape[0] - config.step)
+            # create windowed sample
+
+            X_sample = wav[rand_ind: rand_ind + config.step]
+            # generate mfccs from sample
+            X_mfccs = librosa.feature.mfcc(X_sample, sr, n_mfcc=config.n_mfcc, n_fft=config.n_fft,
+                                           hop_length=config.n_fft)[1:config.n_feat + 1]
+            '''
+            The output of this funciton is the matrix mfcc -> numpy array of shape (n_mfcc, T)
+            T -> track duration 
+            '''
+            # check min and max values
+            _min = min(np.amin(X_mfccs), _min)
+            _max = max(np.amax(X_mfccs), _max)
+            # add features of window to X
+            Xf.append(X_mfccs)
+
+        # Put window data for file into array and scale
+        Xf = np.array(Xf)
+        Xf = (Xf - _min) / (_max - _min)
+
+        # Now that data is scaled, pick out each window from Xf and add that to X
+        for ar in Xf:
+            X.append(ar)
+
+    # Once windows have been taken from every file reshape X
+    X = np.array(X)
+    X = X.reshape(X.shape[0], X.shape[1], X.shape[2], 1)
+    y = to_categorical(y, num_classes=7)
+
+    return X, y
+
+
+# Build the training data
+X, y = build_train_feats()
+y_flat = np.argmax(y, axis=1)
+
+# This will assign slightly more weight to the neutral class
+class_weight = compute_class_weight('balanced', np.unique(y_flat), y_flat)
+
+# Required input shape for CNN
+input_shape = (X.shape[1], X.shape[2], 1)
+
+
+# Define the model
+def create_cnn():
+    model = Sequential()
+    model.add(Conv2D(16, (3, 3), activation='relu', strides=(
+        1, 1), padding='same', input_shape=input_shape))
+    model.add(Conv2D(32, (3, 3), activation='relu',
+                     strides=(1, 1), padding='same'))
+    model.add(Conv2D(64, (3, 3), activation='relu',
+                     strides=(1, 1), padding='same'))
+    model.add(Conv2D(128, (3, 3), activation='relu',
+                     strides=(1, 1), padding='same'))
+    model.add(MaxPool2D((2, 2)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(7, activation='softmax'))
+    model.summary()
+    model.compile(loss='categorical_crossentropy',
+                  optimizer='adam', metrics=['acc'])
+    return model
+
+
+# Instantiate and fit the model
+print('''
+    -------- Training the Model --------
+    ''')
+
+keras_model = create_cnn()
+keras_model.fit(X, y, epochs=25, batch_size=32,
+                shuffle=True, class_weight=class_weight)
+
+
+# Save the model
+pickle.dump(keras_model, open("voice_emotion/Keras/emotion_cnn.pkl", "wb"))
+
+print('''
+    -------- Model Saved --------
+    ''')
