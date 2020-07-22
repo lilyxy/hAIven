becb9b5bf4f4543073f662b51147b4a63c1f91a6
jenny-huaang <40249176+jenny-huaang@users.noreply.github.com>
2020-07-21 23:15:54 -0700

    UI fixes to Calendar Screen

--- haiven-app/app/screens/CalendarScreen.js
+++ haiven-app/app/screens/CalendarScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { View, StyleSheet, SafeAreaView, Text } from "react-native";
-import { Calendar, CalendarList, Agenda } from "react-native-calendars";
+import { View, StyleSheet, SafeAreaView, Text, ScrollView } from "react-native";
+import { Calendar } from "react-native-calendars";
 import colors from "../config/colors";
 import { Footer } from "../components/Footer";
 import { FontAwesome5 } from "@expo/vector-icons";
@@ -24,103 +24,114 @@ export class Insight extends React.Component {
 function CalendarScreen({ navigation }) {
   return (
     <SafeAreaView style={styles.container}>
-      <View>
-        <Calendar
-          onDayPress={(day) => {
-            var dateSelected = new Date(
-              day["year"],
-              day["month"] - 1,
-              day["day"]
-            );
-            navigation.navigate("Journal", {
-              date: dateSelected.toDateString(),
-            });
+      <ScrollView>
+        <View>
+          <Calendar
+            onDayPress={(day) => {
+              var dateSelected = new Date(
+                day["year"],
+                day["month"] - 1,
+                day["day"]
+              );
+              navigation.navigate("Journal", {
+                date: dateSelected.toDateString(),
+              });
+            }}
+            theme={{
+              textSectionTitleColor: colors.primary,
+              monthTextColor: colors.primary,
+              indicatorColor: "blue",
+              selectedDotColor: colors.secondary,
+              selectedDayTextColor: colors.secondary,
+              todayTextColor: colors.primary,
+              arrowColor: colors.primary,
+              textMonthFontWeight: "bold",
+              textMonthFontSize: 30,
+            }}
+            m // Collection of dates that have to be colored in a special way. Default = {}
+            markedDates={{
+              "2020-07-19": {
+                disabled: true,
+                startingDay: true,
+                color: colors.happy,
+                endingDay: true,
+              },
+              "2020-07-20": {
+                disabled: true,
+                startingDay: true,
+                color: colors.happy,
+                endingDay: true,
+              },
+              "2020-07-21": {
+                disabled: true,
+                startingDay: true,
+                color: colors.happy,
+                endingDay: true,
+              },
+              "2020-07-22": {
+                disabled: true,
+                startingDay: true,
+                color: colors.sad,
+                endingDay: true,
+              },
+              "2020-07-23": {
+                disabled: true,
+                startingDay: true,
+                color: colors.angry,
+                endingDay: true,
+              },
+              "2020-07-24": {
+                disabled: true,
+                startingDay: true,
+                color: colors.angry,
+                endingDay: true,
+              },
+              "2020-07-25": {
+                disabled: true,
+                startingDay: true,
+                color: colors.sad,
+                endingDay: true,
+              },
+              "2020-07-26": {
+                disabled: true,
+                startingDay: true,
+                color: colors.happy,
+                endingDay: true,
+              },
+              "2020-07-27": {
+                disabled: true,
+                startingDay: true,
+                color: colors.sad,
+                endingDay: true,
+              },
+            }}
+            // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
+            markingType={"period"}
+          />
+        </View>
+        <View
+          style={{
+            borderBottomColor: colors.primary,
+            borderBottomWidth: 5,
+            width: "50%",
+            marginTop: 10,
+            alignSelf: "center",
           }}
-          theme={{
-            textSectionTitleColor: colors.primary,
-            monthTextColor: colors.primary,
-            indicatorColor: "blue",
-            selectedDotColor: colors.secondary,
-            selectedDayTextColor: colors.secondary,
-            todayTextColor: colors.primary,
-            arrowColor: colors.primary,
-            textMonthFontWeight: "bold",
-            textMonthFontSize: 30,
-          }}
-          m // Collection of dates that have to be colored in a special way. Default = {}
-          markedDates={{
-            "2020-07-19": {
-              disabled: true,
-              startingDay: true,
-              color: colors.sad,
-              endingDay: true,
-            },
-            "2020-07-20": {
-              disabled: true,
-              startingDay: true,
-              color: colors.sad,
-              endingDay: true,
-            },
-            "2020-07-21": {
-              disabled: true,
-              startingDay: true,
-              color: colors.happy,
-              endingDay: true,
-            },
-            "2020-07-22": {
-              disabled: true,
-              startingDay: true,
-              color: colors.sad,
-              endingDay: true,
-            },
-            "2020-07-23": {
-              disabled: true,
-              startingDay: true,
-              color: colors.angry,
-              endingDay: true,
-            },
-            "2020-07-24": {
-              disabled: true,
-              startingDay: true,
-              color: colors.angry,
-              endingDay: true,
-            },
-            "2020-07-25": {
-              disabled: true,
-              startingDay: true,
-              color: colors.sad,
-              endingDay: true,
-            },
-            "2020-07-26": {
-              disabled: true,
-              startingDay: true,
-              color: colors.happy,
-              endingDay: true,
-            },
-            "2020-07-27": {
-              disabled: true,
-              startingDay: true,
-              color: colors.sad,
-              endingDay: true,
-            },
-          }}
-          // Date marking style [simple/period/multi-dot/custom]. Default = 'simple'
-          markingType={"period"}
         />
-      </View>
-      <View style={{ padding: 10 }}>
-        <Text style={styles.subheading}>Insights</Text>
-        <Insight
-          insight="Happiness"
-          icon="smile-beam"
-          text="This month you were on average happier than last month."
-        />
-        <Insight
-          insight="Anger"
-          icon="sad-tear"
-          text="We've noticd that there is usally anger detected Saturday nights."
-        />
-      </View>
+        <View style={{ padding: 10 }}>
+          <Text style={styles.subheading}>Insights</Text>
+          <Insight
+            insight="Happiness"
+            icon="smile-beam"
+            text="This month you were on average happier than last month."
+          />
+          <Insight
+            insight="Anger"
+            icon="sad-tear"
+            text="We've noticd that there is usally anger detected Saturday nights."
+          />
+        </View>
+      </ScrollView>
       <View>
         <Footer />
       </View>
@@ -135,7 +146,7 @@ const styles = StyleSheet.create({
     backgroundColor: colors.background,
   },
   insight: {
-    margin: 15,
+    margin: 10,
     backgroundColor: colors.secondary,
     borderRadius: 10,
     padding: 10,
